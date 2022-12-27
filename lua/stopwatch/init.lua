b488b9c27b82702fea2468c5492beddfe7de251b
Bruno Moretto <bmmcruzalta2@gmail.com>
2022-12-27 00:29:22 -0300

    Changed the string delimiters to single quotes

--- lua/stopwatch/init.lua
+++ lua/stopwatch/init.lua
@@ -5,7 +5,7 @@ local function delta_time()
     local minutes = math.floor(seconds / 60)
     local hours = math.floor(minutes / 60)
 
-    return string.format("%02d:%02d", hours % 60, minutes % 60)
+    return string.format('%02d:%02d', hours % 60, minutes % 60)
 end
 
 return {
