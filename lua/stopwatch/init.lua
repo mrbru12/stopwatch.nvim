23960daf319c9a12b38b470566fe061cb72b5d08
Bruno Moretto <bmmcruzalta2@gmail.com>
2022-12-27 03:05:13 -0300

    Added some basic doc comments to the functions

--- lua/stopwatch/init.lua
+++ lua/stopwatch/init.lua
@@ -1,17 +1,21 @@
 local start_time = os.time()
 
+-- Returns the seconds elapsed since Neovim started
 local function seconds()
     return os.difftime(os.time(), start_time)
 end
 
+-- Returns the minutes elapsed since Neovim started
 local function minutes()
     return math.floor(seconds() / 60)
 end
 
+-- Returns the hours elapsed since Neovim started
 local function hours()
     return math.floor(minutes() / 60)
 end
 
+-- Returns a string formatted as "MINUTES:HOURS" elapsed since Neovim started 
 local function default()
     return string.format('%02d:%02d', hours() % 60, minutes() % 60)
 end
