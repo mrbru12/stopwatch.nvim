8eba85297572efe8eb855edd61b86c7e09a5e7a9
Bruno Moretto <bmmcruzalta2@gmail.com>
2022-12-27 02:13:45 -0300

    Increased user customization powers by abstracting the code

--- lua/stopwatch/init.lua
+++ lua/stopwatch/init.lua
@@ -1,13 +1,24 @@
 local start_time = os.time()
 
-local function delta_time()
-    local seconds = os.difftime(os.time(), start_time)
-    local minutes = math.floor(seconds / 60)
-    local hours = math.floor(minutes / 60)
+local function seconds()
+    return os.difftime(os.time(), start_time)
+end
 
-    return string.format('%02d:%02d', hours % 60, minutes % 60)
+local function minutes()
+    return math.floor(seconds() / 60)
+end
+
+local function hours()
+    return math.floor(minutes() / 60)
+end
+
+local function default()
+    return string.format('%02d:%02d', hours() % 60, minutes() % 60)
 end
 
 return {
-    delta_time = delta_time
+    seconds = seconds,
+    minutes = minutes,
+    hours = hours,
+    default = default
 }
